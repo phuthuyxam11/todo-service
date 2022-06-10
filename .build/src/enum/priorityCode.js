97c96c3c5365290b75bb6091bc97e69209ad0adf
phuthuyxam11 <104954760+phuthuyxam11@users.noreply.github.com>
2022-06-10 16:57:17 +0700

    ik

--- .build/src/enum/priorityCode.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PriorityCode = void 0;
-var PriorityCode;
-(function (PriorityCode) {
-    PriorityCode[PriorityCode["normal"] = 1] = "normal";
-    PriorityCode[PriorityCode["important"] = 2] = "important";
-})(PriorityCode = exports.PriorityCode || (exports.PriorityCode = {}));
-//# sourceMappingURL=priorityCode.js.map
